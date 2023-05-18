d60f726fe1816624a03ddbf0ad49d96152f1da2f
Luca Saverio Esposito <esposito.1917959@studenti.uniroma1.it>
2023-05-18 12:01:46 +0200

    First commit, removed test classes and added first dummy test in tajo:client

---
+++ tajo-client/src/test/java/isw/first/test/firstTest.java
@@ -0,0 +1,54 @@
+package isw.first.test;
+
+import org.apache.tajo.client.CatalogAdminClientImpl;
+import org.apache.tajo.client.SessionConnection;
+import org.apache.tajo.exception.DuplicateDatabaseException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class firstTest {
+
+    private static final String MOCKITO_LABEL = "Mockito test, let's get it !";
+    private static final String DB_LABEL = " exist";
+
+    @Mock
+    CatalogAdminClientImpl catalogAdminClient;
+
+    @Before
+    public void createMocks() {
+        final String dbName2 = "DB_pazzo";
+        final String dbName = "DB_serio";
+        // add the mocked behavior for two DB names
+        when(catalogAdminClient.existDatabase(dbName2)).thenReturn(true);
+        when(catalogAdminClient.existDatabase(dbName)).thenReturn(true);
+    }
+
+    @Test
+    public void testOne(){
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void mockitoTest(){
+        String dbN = "DB_pazzo";
+        String dbN2 = "DB_serio";
+
+        boolean b1 = catalogAdminClient.existDatabase(dbN);
+        boolean b2 = catalogAdminClient.existDatabase(dbN2);
+        boolean b3 = catalogAdminClient.existDatabase("DB_pippo");
+
+        boolean condition = b1 && b2 && !b3;
+        Assert.assertTrue(condition);
+    }
+
+}
